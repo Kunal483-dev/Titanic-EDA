9431bf1c2df42cd2147e3200cbf22d965563e4a6
Kunal483-dev <rajpootkunal969@gmail.com>
2026-08-30 11:27:32 +0530

    Updated EDA
    I loaded the cleaned Titanic dataset using Pandas.

--- EDA.py
+++ EDA.py
@@ -1,7 +1,12 @@
-# Step 1: Import required libraries
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-sns.set_theme(style="whitegrid")
+file_path = r"C:\Users\rajpo\Desktop\Titanic-Cleaned-Dataset.csv"
+df = pd.read_csv(file_path)
+
+print("Dataset shape:", df.shape)
+print("\nFirst five rows:")
+print(df.head())
